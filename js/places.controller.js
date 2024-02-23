99eae40117c2289be5e7e8ce1bff20f60e67d133
Atar Mor <atarmor92@gmail.com>
2024-02-23 22:47:48 +0200

    Add map & add new place & go to place

--- js/places.controller.js
+++ js/places.controller.js
@@ -1,15 +1,18 @@
 'use strict'
 
+var gMap
+
 function onInit() {
     renderPlaces()
+    initMap()
 }
 
 function renderPlaces() {
     const places = getPlaces()
     const strHTMLs = places.map(place => {
         return `<li> ${place.name}
-                    <button onclick = "onRemovePlace('${place.id}')">X</button>
-                    <button onclick="onShowPlace()">Go</button>
+                    <button onclick = "onRemovePlace(${place.id})">X</button>
+                    <button onclick="onPanToPlace(${place.id})">Go</button>
                 </li>`
     })
     document.querySelector('.places-list span').innerHTML = strHTMLs.join('')
@@ -18,4 +21,35 @@ function renderPlaces() {
 function onRemovePlace(placeId) {
     removePlace(placeId)
     renderPlaces()
+}
+
+function initMap(lat = 29.557669, lng = 34.951923) {
+
+    const elMap = document.querySelector('.map')
+    const mapOptions = {
+        center: { lat, lng },
+        zoom: 10
+    }
+    gMap = new google.maps.Map(elMap, mapOptions)
+
+    gMap.addListener('click', ev => {
+        const name = prompt('Place name?', 'Place 1')
+        const lat = ev.latLng.lat()
+        const lng = ev.latLng.lng()
+        addPlace(name, lat, lng, gMap.getZoom())
+        renderPlaces()
+    })
+
+    const markerOptions = {
+        position: { lat, lng },
+        gMap,
+        title: 'Eilat'
+    }
+    const marker = new google.maps.Marker(markerOptions)
+}
+
+function onPanToPlace(placeId) {
+    const place = getPlaceById(placeId)
+    gMap.setCenter({ lat: place.lat, lng: place.lng })
+    gMap.setZoom(place.zoom)
 }
--- js/services/place-service.js
+++ js/services/place-service.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const gPlaces = [
-    {id: '1p2', lat: 32.1416, lng: 34.831213, name: 'Pukis house'},
-    {id: '1p3', lat: 32.4416, lng: 34.931213, name: 'house'}
-]
+var gIdx = 101
+
+var gPlaces
+_createPlaces()
 
 function getPlaces() {
     return gPlaces
@@ -12,4 +12,31 @@ function getPlaces() {
 function removePlace(placeId) {
     const placeIdx = gPlaces.findIndex(place => place.id === placeId)
     gPlaces.splice(placeIdx, 1)
+}
+
+function addPlace(name, lat, lng, zoom) {
+    const place = _createPlace(name, lat, lng, zoom)
+    gPlaces.push(place)
+}
+
+function getPlaceById(placeId) {
+    return gPlaces.find(place => place.id === placeId)
+}
+
+function _createPlaces() {
+    gPlaces = [
+        _createPlace('Pukis house', 32.1416, 34.831213),
+        _createPlace('house', 32.4416, 34.931213),
+        _createPlace('Eilat', 29.557669, 34.951923)
+    ]
+}
+
+function _createPlace(name, lat, lng, zoom = 10) {
+    return {
+        id: gIdx++,
+        lat,
+        lng,
+        name,
+        zoom
+    }
 }
